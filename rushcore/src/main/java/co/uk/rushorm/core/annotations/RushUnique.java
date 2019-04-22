54999f3334d63bc56357517cfd3c3b8016d4b622
Pascal <meteo66240@free.fr>
2019-04-22 10:19:09 +0200

    Start Unique Field

---
+++ rushcore/src/main/java/co/uk/rushorm/core/annotations/RushUnique.java
@@ -0,0 +1,12 @@
+/*
+ * Copyright Priveyes (c) 2019.
+ */
+
+package co.uk.rushorm.core.annotations;
+
+import java.lang.annotation.*;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.FIELD)
+public @interface RushUnique {
+}
